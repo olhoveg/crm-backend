eb3075e3621b4a00d23c0c6dbc51f46c799bdcc7
olhoveg@gmail.com <olhoveg@gmail.com>
2025-06-14 23:02:32 +0300

    Правки

--- index.js
+++ index.js
@@ -139,8 +139,11 @@ app.post('/api/deals', async (req, res) => {
     // Проверь порядок полей и переменных!
     const result = await pool.query(
       `INSERT INTO deals (
-        title, client_id, service_type, comment, status, desired_date
-      ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+  title, client_id, responsible_id, budget, status, comment, reason, lawyer,
+  contract_number, service_type, desired_date, first_contact_at, reaction_time, nps, nps_comment
+) VALUES (
+  $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
+) RETURNING *`,
       [title, client_id, service_type, comment, status, desired_date]
     );
 
